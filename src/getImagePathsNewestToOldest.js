687cc621cc3536638cb8e2b83fcdb6ea597427dc
lukix <lukix.95@gmail.com>
2022-09-12 21:27:27 +0200

    Add filtering fileNames by extension

--- src/getImagePathsNewestToOldest.js
+++ src/getImagePathsNewestToOldest.js
@@ -5,7 +5,9 @@ const readDir = promisify(fs.readdir);
 
 const getImagePathsNewestToOldest = async ({ imagesDirName }) => {
   const filesList = await readDir(imagesDirName);
-  return filesList.sort((a, b) => b.localeCompare(a));
+  return filesList
+    .filter(fileName => fileName.endsWith('.jpg'))
+    .sort((a, b) => b.localeCompare(a));
 }
 
 module.exports = getImagePathsNewestToOldest;
